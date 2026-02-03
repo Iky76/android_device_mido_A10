a0ddeed42842d609c8ae87a682d66a143541b3e9
Iky76 <yuyenem@gmail.com>
2026-02-03 23:03:32 +0700

    Update product makefiles and lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rr_mido.mk
+    $(LOCAL_DIR)/qassa_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    rr_mido-user \
-    rr_mido-userdebug \
-    rr_mido-eng
+    qassa_mido-user \
+    qassa_mido-userdebug \
+    qassa_mido-eng
