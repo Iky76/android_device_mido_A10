564fe9c2e5cb8772c24b1155a0f70e874e85c24e
Iky76 <yuyenem@gmail.com>
2026-04-09 23:01:55 +0700

    Comment out SELINUX_IGNORE_NEVERALLOWS
    Comment out SELINUX_IGNORE_NEVERALLOWS to allow build.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,7 +206,7 @@ BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # We modify several neverallows, so let the build proceed
 ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
+# SELINUX_IGNORE_NEVERALLOWS := true
 endif
 
 # Treble
